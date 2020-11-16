2b18f0bd084d018ab1e697df69e7f813f87db1e8
slater0013 <guenael.sanchez@gmail.com>
2020-11-16 20:23:07 +0100

    seed

---
+++ vmhopromexp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#VMWare Horizon Prometheus Exporter
+
+#GS - Nov 16 2020
+
+#USAGE : vmhopromexp --horizon_endpoint="https://horizon_endpoint.fqdn/" --horizon_user="user@vspehere_domain" --horizon_password="*********"
