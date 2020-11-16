a0bf5f329dc933374d62c09263ebad065ed6d53e
slater0013 <guenael.sanchez@gmail.com>
2020-11-16 21:45:54 +0100

    usage

--- vmhopromexp.py
+++ vmhopromexp.py
@@ -12,18 +12,19 @@ import getopt
 def connect_horizon_endpoint(horizon_endpoint,horizon_user,horizon_password):
     print("Connexion au serveur : "+horizon_endpoint+" "+horizon_user+"/"+horizon_password)
 
+def usage():
+    print 'Usage : vmhopromexp.py --horizon_endpoint="https://horizon_endpoint.fqdn/" --horizon_user="user@vspehere_domain" --horizon_password="*********"'
 
 def main(argv):
 
     horizon_endpoint=""
     horizon_user=""
     horizon_password=""
-    usage='Usage : vmhopromexp.py --horizon_endpoint="https://horizon_endpoint.fqdn/" --horizon_user="user@vspehere_domain" --horizon_password="*********"'
 
     try:
             opts, args = getopt.getopt(argv,"e:u:p:",["horizon_endpoint=","horizon_user=","horizon_password="])
     except getopt.GetoptError:
-            print usage
+            usage()
             sys.exit(2)
     for opt, arg in opts:
         if opt in ("-e","--horizon_endpoint"):
@@ -33,15 +34,13 @@ def main(argv):
         elif opt in ("-p","--horizon_password"):
             horizon_password=arg
         else:
-            print usage
             usage()
             sys.exit(2)
 
     if(horizon_endpoint == "" or horizon_user == "" or horizon_password == ""):
-        print usage
         usage()
         sys.exit(2)
-        
+
     connect_horizon_endpoint(horizon_endpoint,horizon_user,horizon_password)
 
 main(sys.argv[1:])
